f5fd17dc8cd56da97c28ec33cb0aa83362580883
tonipulkkinen <chipruli@gmail.com>
2017-04-05 11:16:19 +0300

    Created contact-list component and linked it to app component. Created empty button for adding contacts.

--- ng-app-client/src/app/app.module.ts
+++ ng-app-client/src/app/app.module.ts
@@ -2,17 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import {ContactListComponent} from './contact/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    FlexLayoutModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
