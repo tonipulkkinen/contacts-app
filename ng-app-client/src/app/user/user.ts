7290712f06d0d367b37a9ca52016ded8ff8a037d
tonipulkkinen <chipruli@gmail.com>
2017-04-19 10:01:41 +0300

    Login page layout ready.

--- ng-app-client/src/app/user/user.ts
+++ ng-app-client/src/app/user/user.ts
@@ -1,2 +1,14 @@
 export class User {
+
+  _firstName: string;
+  _lastName: string;
+  _username: string;
+  _password: string;
+
+  constructor(firstName?: string, lastName?: string, username?: string, password?: string) {
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._username = username;
+    this._password = password;
+  }
 }
