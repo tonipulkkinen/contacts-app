3034782dc24e4470e673ff9918bbc93388d7f56c
tonipulkkinen <chipruli@gmail.com>
2017-05-09 13:29:44 +0300

    Small cleanup.

--- ng-app-client/src/app/contact/utils/vibration.directive.spec.ts
+++ ng-app-client/src/app/contact/utils/vibration.directive.spec.ts
@@ -1,8 +1,5 @@
 import { VibrationDirective } from './vibration.directive';
 
 describe('VibrationDirective', () => {
-  it('should create an instance', () => {
-    const directive = new VibrationDirective();
-    expect(directive).toBeTruthy();
-  });
+
 });
